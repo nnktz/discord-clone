8357570536f83b9499872f78d10ba67ec8e46f41
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-17 17:43:54 +0700

    initial modal ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import { ThemeProvider } from '@/components/ui/providers/theme-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
