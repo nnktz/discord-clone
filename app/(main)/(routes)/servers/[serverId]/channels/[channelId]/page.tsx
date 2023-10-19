2880468e59d1c1ba80ba11cbbaf8163b9f9b5b61
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-19 19:49:45 +0700

    chat header & menu navbar mobile

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,50 @@
-const ChannelIdPage = () => {
-  return <div>hhi</div>;
+import { redirect } from 'next/navigation';
+import { redirectToSignIn } from '@clerk/nextjs';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { ChatHeader } from '@/components/chat/chat-header';
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage: React.FC<ChannelIdPageProps> = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect('/');
+  }
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={params.serverId}
+        type='channel'
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
