f967fe3e6eddce65c6046722f60495dcc9858c82
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-21 17:33:47 +0700

    chat messages component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { ChatHeader } from '@/components/chat/chat-header';
 import { ChatInput } from '@/components/chat/chat-input';
+import { ChatMessages } from '@/components/chat/chat-messages';
 
 interface ChannelIdPageProps {
   params: {
@@ -45,7 +46,18 @@ const ChannelIdPage: React.FC<ChannelIdPageProps> = async ({ params }) => {
         type='channel'
       />
 
-      <div className='flex-1'>mes</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type='channel'
+        apiUrl='/api/messages'
+        socketUrl='/api/socket/messages'
+        socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+        paramKey='channelId'
+        paramValue={channel.id}
+      />
+
       <ChatInput
         name={channel.name}
         type='channel'
